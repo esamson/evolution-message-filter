cc0f1e9c20c026d23ae8f9633e5e3e70a3967cf1
Edward Samson <esamson@ewise.com>
2015-02-06 10:16:22 +0800

    Repurpose java version for tracing filtered mail

--- src/main/java/ph/samson/mail/filter/Trace.java
+++ src/main/java/ph/samson/mail/filter/Trace.java
@@ -10,26 +10,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * Output filtered mail to logs.
  *
  * @author Edward Samson
  */
-public class Main {
+public class Trace {
     
-    private static final int CALENDAR = 1;
     private static final Logger log
-            = LoggerFactory.getLogger(Main.class);
+            = LoggerFactory.getLogger(Trace.class);
+    private static final Logger msg
+            = LoggerFactory.getLogger("msg");
 
     public static void main(String[] args) {
+        log.info("Message START");
         try (BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                if (line.startsWith("Content-Type: text/calendar")) {
-                    System.exit(CALENDAR);
-                }
+                msg.info(line);
             }
+            log.info("Message END");
         } catch (IOException ex) {
             log.error("Fail", ex);
+            System.exit(-1);
         }
+        System.exit(1);
     }
 }
